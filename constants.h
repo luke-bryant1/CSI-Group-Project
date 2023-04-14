74260ff634b9ef31437946f655800ce86f5461cc
luke-bryant1 <123609480+luke-bryant1@users.noreply.github.com>
2023-04-14 18:29:53 -0500

    Update constants.h

--- constants.h
+++ constants.h
@@ -1,14 +1,19 @@
 #ifndef CONSTANTS_H_INCLUDED
 #define CONSTANTS_H_INCLUDED
 
-#include "SDL_plotter.h"
-
-const int NUM_ROWS = 800;
-const int NUM_COL = 500;
+const int NUM_ROW = 500;
+const int NUM_COL = 400;
 
 const int NUM_TILES = 4;
+const int TILE_SIZE = 20;
+
+const int SPEED = 150; //This controls the speed that the block falls (the higher the number the slower)
 
 const color RED   = color(220, 20, 20);
+const color ORANGE   = color(255, 165, 0);
+const color YELLOW   = color(220, 250, 0);
+const color GREEN   = color(20, 220, 20);
+const color BLUE   = color(20, 20, 220);
 const color BLACK = color(0, 0, 0);
 const color BACKGROUND = color(255, 255, 255);
 
@@ -16,6 +21,4 @@ enum blockType {bar, square, l_shape, t_shape, z_shape};
 enum direction {north, east, south, west};
 
 
-
 #endif // CONSTANTS_H_INCLUDED
-//test
