d62f9290a801f9cd21bb69a994ad8721349bc4fc
luke-bryant1 <123609480+luke-bryant1@users.noreply.github.com>
2023-04-14 18:30:25 -0500

    Update block.h

--- block.h
+++ block.h
@@ -2,18 +2,34 @@
 #define BLOCK_H_INCLUDED
 
 #include "tile.h"
-#include "SDL_Plotter.h"
 
 class Block{
-
     private:
         tile data[NUM_TILES];
         point loc;
         color shade;
         blockType type;
+        int size;
 
     public:
         Block();
+
+        point getLocation() const;
+        color getColor() const;
+        int getSize() const;
+
+        void setLocation(const point&);
+        void setColor(const color&);
+        void setSize(int);
+        void setType(blockType);
+        void switchType();
+        void update(SDL_Plotter& g);
+
+        void draw(SDL_Plotter& g);
+        void move();
+        void moveLeft();
+        void moveRight();
+        void moveDown();
 
 
 };
