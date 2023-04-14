7105f4d32b0c4b7ad602c5b403bb8896dac9ea70
luke-bryant1 <123609480+luke-bryant1@users.noreply.github.com>
2023-04-14 18:27:34 -0500

    Update tile.cpp

--- tile.cpp
+++ tile.cpp
@@ -11,29 +11,34 @@ void tile::moveRight(){
     p.x += size; //moves the tile right by one tile
     setLocation(p);
 }
-
-void tile::move(){
+void tile::moveDown(){
     point p = getLocation();
-    p.y++;
+    p.y += size; //moves the tile down by one tile
     setLocation(p);
 }
 
-void tile::draw(SDL_Plotter& g){ //"tile::" adds everything in object tile to the function
+void tile::move(){
+    point p = getLocation();
+    p.y += size; //makes it fall one tile space at a time
+    setLocation(p);
+}
+
+void tile::draw(SDL_Plotter& g){
     color currColor;
     //erase
     for(int r = 0; r < size; r++){ //"size" comes from inside the object
         for(int c = 0; c < size; c++){
-            g.PlotPixel(prevLoc.x + c, prevLoc.y + r, BACKGROUND); //this erases the previous location
+            g.plotPixel(prevLoc.x + c, prevLoc.y + r, BACKGROUND); //this erases the previous location
         }
     }
     //draw
-    for(int t = 0; r < size; r++){ //"size" comes from inside the object
+    for(int r = 0; r < size; r++){ //"size" comes from inside the object
         for(int c = 0; c < size; c++){
-            currentColor = shade;
+            currColor = shade;
             if(c == 0 || r == 0 || r == size - 1 || c == size - 1){
                 currColor = BLACK; //This adds a black boarder around the edge of the tile
             }
-            g.PlotPixel(loc.x + c, loc.y + r, currColor); //this draws the square
+            g.plotPixel(loc.x + c, loc.y + r, currColor); //this draws the square
         }
     }
     prevLoc = loc;
@@ -53,7 +58,7 @@ void tile::setLocation(const point& p){
 //    prevLoc = loc; //This sets the previous location to loc before it updates loc
 //    we had to to move this to the last place that it was drawn so it wouldn't leave a streak
     if(p.x >= 0 && p.y>= 0 &&
-       p.x < NUM_COL - size && p.y < NUM_ROWS - size){
+       p.x < NUM_COL - size && p.y < NUM_ROW - size){
         loc = p;
     }
     return;
@@ -65,4 +70,7 @@ void tile::setColor(const color& c){
 void tile::setSize(int s){
     size = s;
     return;
+}
+void tile::update(SDL_Plotter& g){
+    g.update();
 }
