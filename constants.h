1a4e265af8e70a0b23400921c960fe4ea16e0b45
luke-bryant1 <123609480+luke-bryant1@users.noreply.github.com>
2023-04-18 23:42:19 -0500

    Update constants.h

--- constants.h
+++ constants.h
@@ -4,17 +4,18 @@
 const int NUM_ROW = 500;
 const int NUM_COL = 400;
 
-const int NUM_TILES = 4;
-const int TILE_SIZE = 20;
+const int NUM_TILES     = 4;
+const int TILE_SIZE     = 25;
+const int NUM_OF_BLOCKS = 50;
 
 const int SPEED = 150; //This controls the speed that the block falls (the higher the number the slower)
 
-const color RED   = color(220, 20, 20);
-const color ORANGE   = color(255, 165, 0);
-const color YELLOW   = color(220, 250, 0);
-const color GREEN   = color(20, 220, 20);
-const color BLUE   = color(20, 20, 220);
-const color BLACK = color(0, 0, 0);
+const color RED        = color(220, 20, 20);
+const color ORANGE     = color(255, 165, 0);
+const color YELLOW     = color(220, 250, 0);
+const color GREEN      = color(20, 220, 20);
+const color BLUE       = color(20, 20, 220);
+const color BLACK      = color(0, 0, 0);
 const color BACKGROUND = color(255, 255, 255);
 
 enum blockType {bar, square, l_shape, t_shape, z_shape};
