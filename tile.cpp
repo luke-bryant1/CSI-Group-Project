c4440da5f54e4abdbc79e8f7a5c35c8c82c61465
luke-bryant1 <123609480+luke-bryant1@users.noreply.github.com>
2023-04-26 16:17:05 -0500

    Update tile.cpp

--- tile.cpp
+++ tile.cpp
@@ -1,29 +1,76 @@
-#ifndef CONSTANTS_H_INCLUDED
-#define CONSTANTS_H_INCLUDED
-
-const int NUM_ROW = 500;
-const int NUM_COL = 400;
-
-const int NUM_TILES     = 4;
-const int TILE_SIZE     = 25;
-const int NUM_OF_BLOCKS = 50;
-const int STARTING_X    = NUM_ROW / 3;
-const int STARTING_Y    = TILE_SIZE * 4;
+#include "tile.h"
 
 
-const int SPEED = 150; //This controls the speed that the block falls (the higher the number the slower)
+void tile::moveLeft(){
+    point p = getLocation();
+    p.x -= size; //moves the tile left by one tile
+    setLocation(p);
+}
+void tile::moveRight(){
+    point p = getLocation();
+    p.x += size; //moves the tile right by one tile
+    setLocation(p);
+}
+void tile::moveDown(){
+    point p = getLocation();
+    p.y += size; //moves the tile down by one tile
+    setLocation(p);
+}
 
-const color RED        = color(220, 20, 20);
-const color ORANGE     = color(255, 165, 0);
-const color YELLOW     = color(220, 250, 0);
-const color GREEN      = color(20, 220, 20);
-const color BLUE       = color(20, 20, 220);
-const color BLACK      = color(0, 0, 0);
-const color BACKGROUND = color(255, 255, 255);
+void tile::move(){
+    point p = getLocation();
+    p.y += size; //makes it fall one tile space at a time
+    setLocation(p);
+}
 
-enum blockType {bar, square, l_shape, t_shape, z_shape};
-enum direction {north, east, south, west};
+void tile::draw(SDL_Plotter& g){
+    color currColor;
+    //erase
+    for(int r = 0; r < size; r++){ //"size" comes from inside the object
+        for(int c = 0; c < size; c++){
+            g.plotPixel(prevLoc.x + c, prevLoc.y + r, BACKGROUND); //this erases the previous location
+        }
+    }
+    //draw
+    for(int r = 0; r < size; r++){ //"size" comes from inside the object
+        for(int c = 0; c < size; c++){
+            currColor = shade;
+            if(c == 0 || r == 0 || r == size - 1 || c == size - 1){
+                currColor = BLACK; //This adds a black boarder around the edge of the tile
+            }
+            g.plotPixel(loc.x + c, loc.y + r, currColor); //this draws the square
+        }
+    }
+    prevLoc = loc;
+}
 
+point tile::getLocation() const{
+    return loc;
+}
+color tile::getColor() const{
+    return shade;
+}
+int tile::getSize() const{
+    return size;
+}
 
-
-#endif // CONSTANTS_H_INCLUDED
+void tile::setLocation(const point& p){
+//    prevLoc = loc; //This sets the previous location to loc before it updates loc
+//    we had to to move this to the last place that it was drawn so it wouldn't leave a streak
+    if(p.x >= 0 && p.y>= 0 &&
+       p.x < NUM_COL - size && p.y < NUM_ROW - size){
+        loc = p;
+    }
+    return;
+}
+void tile::setColor(const color& c){
+    shade = c;
+    return;
+}
+void tile::setSize(int s){
+    size = s;
+    return;
+}
+void tile::update(SDL_Plotter& g){
+    g.update();
+}
