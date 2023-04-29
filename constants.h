d8a961531da13e4db098b1e33e1138653d867036
luke-bryant1 <123609480+luke-bryant1@users.noreply.github.com>
2023-04-29 00:20:54 -0500

    Update constants.h

--- constants.h
+++ constants.h
@@ -6,23 +6,25 @@ const int ROW = 25;
 
 const int NUM_TILES     = 4;
 const int TILE_SIZE     = 25;
-const int NUM_ROW = TILE_SIZE * ROW;
+const int NUM_ROW = TILE_SIZE * (ROW);
 const int NUM_COL = TILE_SIZE * COL;
 
 const int STARTING_X    = TILE_SIZE * 4;
-const int STARTING_Y    = TILE_SIZE * 2;
+const int STARTING_Y    = TILE_SIZE * 3;
 
 const point START_POINT(STARTING_X, STARTING_Y);
 
 const int SPEED = 150; //This controls the speed that the block falls (the higher the number the slower)
 
-const color RED        = color(220, 20, 20);
+const color RED        = color(255, 0, 0);
 const color ORANGE     = color(255, 165, 0);
-const color YELLOW     = color(220, 250, 0);
-const color GREEN      = color(20, 220, 20);
-const color BLUE       = color(20, 20, 220);
+const color YELLOW     = color(255, 255, 0);
+const color GREEN      = color(0, 255, 0);
+const color CYAN       = color(0, 255, 255);
+const color BLUE       = color(0, 0, 255);
+const color PURPLE     = color(128, 0, 128);
 const color BLACK      = color(0, 0, 0);
-const color BACKGROUND = color(255, 255, 255);
+const color BACKGROUND = color(127, 127, 127);
 
 enum blockType {bar, square, l_shape, t_shape, z_shape};
 enum direction {north, east, south, west};
