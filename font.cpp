15cbed55d3299a3e8dd272ad91b4af898c654460
luke-bryant1 <123609480+luke-bryant1@users.noreply.github.com>
2023-04-29 20:54:54 -0500

    Add files via upload

---
+++ font.cpp
@@ -0,0 +1,76 @@
+#include "font.h"
+
+void letter::init(string fname){
+    ifstream input;
+    input.open(fname);
+    assert(input);
+
+    input >> row >> col;
+    for(int r = 0; r < row; r++){
+        for (int c = 0; c < col; c++){
+            input >> data[r][c];
+        }
+    }
+    input.close();
+}
+
+void letter::display(ostream& out){
+    for(int r = 0; r < row; r++){
+        for(int c = 0; c < col; c++){
+            if(data[r][c]== '1'){
+                out << data[r][c];
+            }
+            else{
+                out << ' ';
+            }
+        }
+         out << endl;
+    }
+}
+
+void letter::draw(int n, point p, SDL_Plotter& g){
+    for(int r = 0; r < row; r++){
+        for(int c = 0; c < col; c++){
+            if(data[r][c] == '1'){
+                for(int xOff = 0; xOff < n; xOff++){
+                    for(int yOff = 0; yOff < n; yOff++){
+                        g.plotPixel(xOff + p.x + c * n, yOff + p.y + r * n,0,0,0);
+                        g.plotPixel(NUM_COL-200, 50,0,0,0);
+                    }
+                }
+            }
+        }
+    }
+}
+
+Font::Font(){
+    string fname;
+    for(char c = 'A'; c <= 'Z'; c++){
+        fname = "font\\";
+        fname += c;
+        fname += ".txt";
+        data[c].init(fname);
+        cout << fname << endl;
+    }
+    loc.x = 0;
+    loc.y = 0;
+    size = 1;
+}
+
+void Font::setLoc(point p){
+    loc = p;
+}
+
+void Font::setSize(int n){
+    if(n >= 1){
+        size = n;
+    }
+}
+
+void Font::display(char c, ostream& out){
+    data[c].display(out);
+}
+
+void Font::draw(char c, SDL_Plotter& g){
+    data[c].draw(size,loc, g);
+}
---
+++ font.h
@@ -0,0 +1,47 @@
+#ifndef FONT_H_INCLUDED
+#define FONT_H_INCLUDED
+
+
+#include "tetris.h"
+#include "SDL_Plotter.h"
+
+using namespace std;
+
+class letter{
+    private:
+        int row = 0, col = 0;
+        char data[MAX_FONT_SIZE][MAX_FONT_SIZE];
+
+    public:
+        letter(){;}
+        void init (string );
+
+        void display(ostream& );
+
+        void draw(int, point, SDL_Plotter&);
+
+};
+
+class Font{
+    private:
+        letter data[256];
+        point loc;
+        int size;
+
+    public:
+        Font();
+
+        void setLoc(point);
+
+        void setSize(int);
+
+        void display(char, ostream& out = cout);
+
+        void draw(char, SDL_Plotter& g);
+
+
+
+
+};
+
+#endif // FONT_H_INCLUDED
