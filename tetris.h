b988d197a43957f2aab9544045b3e339f01840b2
samuelchoi0522 <57381768+samuelchoi0522@users.noreply.github.com>
2023-04-29 21:00:42 -0500

    Update tetris.h

--- tetris.h
+++ tetris.h
@@ -1,29 +1,22 @@
 #ifndef TETRIS_H_INCLUDED
 #define TETRIS_H_INCLUDED
 
-
-#include <ctime>
-#include <stdlib.h>
-#include <string>
-#include <fstream>
-#include <cassert>
-#include <iostream>
-
 #include "SDL_Plotter.h"
 #include "constants.h"
 #include "tile.h"
 #include "block.h"
-#include "font.h"
 
-using namespace std;
 
 class Tetris{
     private:
-        tile board[ROW][COL];
-        Block currentBlock;
+        
+        
+        int line = 0;
+        int score = 0;
 
     public:
-
+        tile board[ROW][COL];
+        Block currentBlock;
         void setBoard(SDL_Plotter& g);
         void runTetris(SDL_Plotter& g);
 
@@ -31,8 +24,12 @@ class Tetris{
         void updateBoard(SDL_Plotter& g);
 
         void checkForFullRow(SDL_Plotter& g);
-
-        void displayStartScreen(SDL_Plotter& g);
+    
+        void setLine(int);
+        int getLine();
+    
+        void setScore(int);
+        int getScore();
 
 };
 
